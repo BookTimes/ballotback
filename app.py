fba4b9d7bf151d35b93a15d888184a4f688b78ac
Mahadev Maneesh <maha040963@sisharjah.com>
2025-04-22 16:01:35 +0530

    html trial

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Render + Flask!"
+    with open('index.html') as f:
+        return f.read()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
